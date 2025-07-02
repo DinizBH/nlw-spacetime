fabc0629b3f59a112efa566348da0f0a40cf6006
Giovane <91229782+DinizBH@users.noreply.github.com>
2025-07-02 02:10:15 -0300

    HomePage Mobile and New Depencencies

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 import './global.css';
+import blurBg from './src/assets/bg-blur.png';
+import Stripes from './src/assets/stripes.svg';
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg';
+import { cssInterop } from 'nativewind';
+cssInterop(Stripes, { className: 'style' });
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
     <>
-      <View className="flex-1 bg-slate-800">
-        <Text className="mt-4 text-center text-2xl font-bold text-white">Welcome to My App!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <ImageBackground
+        source={blurBg}
+        className="py-10sssss relative flex-1 items-center bg-gray-900 px-8"
+        imageStyle={{ position: 'absolute', left: '-100%', right: 0, top: 0, bottom: 0 }}>
+        <Stripes className="absolute left-2" />
+
+        <View className="flex-1 items-center justify-center gap-6">
+          <NLWLogo />
+          <View className="space-y-2 px-8 text-center">
+            <Text className="font-title text-center text-2xl leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </Text>
+            <Text className="font-body text-center text-base leading-relaxed text-gray-100">
+              Colecione momentos marcantes da sua jornada e compartilhe com o mundo!
+            </Text>
+          </View>
+          <TouchableOpacity activeOpacity={0.7} className="rounded-full bg-green-500 px-5 py-3">
+            <Text className="font-alt text-sm uppercase text-black">Cadastrar Lembrança</Text>
+          </TouchableOpacity>
+        </View>
+        <Text className="font-body text-center text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da Rocketseat
+        </Text>
+        <StatusBar style="light" translucent />
+      </ImageBackground>
     </>
   );
 }
