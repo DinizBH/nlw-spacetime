1a98e9b1ed75923bbe54f9de41d0825c39281ae4
Giovane <91229782+DinizBH@users.noreply.github.com>
2025-07-03 01:22:33 -0300

    feat: create `github oauth routes` and validate APIs for authentication

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,20 @@
+import "dotenv/config";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
+import jwt from "@fastify/jwt";
 
 const app = fastify();
 
 app.register(cors, {
   origin: true, // todas as URLs de front-end podem acessar o backend
 });
+
+app.register(jwt, {
+  secret: "spacetime",
+});
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,10 @@
+{/* Copyright */}
+export default function Copyright() {
+    return (
+    <>
+      <div className="text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da{' '}
+        <a href="https://rocketseat.com.br" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-100">Rocketseat</a>
+      </div>
+  </>)
+}
